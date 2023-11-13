4a2fdfdde526c8e022c794e15208f00a5aff4cf7
Ketlyn de  Melo <ketlynmelo002@gmail.com>
2023-11-13 13:19:34 -0300

    post de dados

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-const express = require ("express")
+const express = require("express")
 const path = require("path")
 
 const app = express()
 
 const caminhoBase = path.join(__dirname, "templates") 
 
-app.get('/usuario/:id', (requisicao, resposta) => {
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.post('/cadastrar/salvar', (requisicao, resposta) =>{
+    console.log(requisicao.body)
+})
+
+app.get('/cadastrar', (requisicao, resposta) =>{
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
+
+app.get('/usuarios/:id', (requisicao, resposta ) => {
     const id = requisicao.params.id
 
-    console.log(`Acessando dados do usuário ${id}`)
+    console.log(`Acessando dados do usuario ${id}`)
 
-    resposta.sendFile(`${caminhoBase}/usuario.html`)
-
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
 })
 
-app.get('/',(resquisicao, resposta) => {
-   resposta.sendFile(`${caminhoBase}/index.html`)
-
-
+app.get('/', (requisicao, resposta) => {
+    resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
 })
-
